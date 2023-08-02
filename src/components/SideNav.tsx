58bda7693d8b05dbccd46e22b89858b747622a9d
Jonathon Higham <109483370+jsshigham@users.noreply.github.com>
2023-08-02 14:53:05 -0500

    fixed log in/out mobile

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -36,10 +36,12 @@ function SideNav() {
           <li>
             <IconHoverEffect>
               <span className="flex items-center gap-4">
-                <VscSignIn className="h-8 w-8 fill-green-700" />
-                <span className="hidden text-lg text-green-700 md:inline">
-                  <button onClick={() => void signIn()}>Sign In</button>
-                </span>
+                <button onClick={() => void signIn()}>
+                  <VscSignIn className="h-8 w-8 fill-green-700" />
+                  <span className="hidden text-lg text-green-700 md:inline">
+                    Sign In
+                  </span>
+                </button>
               </span>
             </IconHoverEffect>
           </li>
@@ -47,10 +49,12 @@ function SideNav() {
           <li>
             <IconHoverEffect>
               <span className="flex items-center gap-4">
-                <VscSignOut className="h-8 w-8 fill-red-500" />
-                <span className="hidden text-lg text-red-500 md:inline">
-                  <button onClick={() => void signOut()}>Sign Out</button>
-                </span>
+                <button onClick={() => void signOut()}>
+                  <VscSignOut className="h-8 w-8 fill-red-500" />
+                  <span className="hidden text-lg text-red-500 md:inline">
+                    Sign Out
+                  </span>
+                </button>
               </span>
             </IconHoverEffect>
           </li>
